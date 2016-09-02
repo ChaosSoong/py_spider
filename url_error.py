c87e358d28b51d133b0505885ba3496603244e24
chaos <13313020563@163.com>
2016-09-02 15:53:44 +0800

    update url_error

--- url_error.py
+++ url_error.py
@@ -1,29 +1,24 @@
-# -*-coding:utf-8 -*-
-import string, urllib2  
-   
-#定义百度函数  
-def baidu_tieba(url,begin_page,end_page):     
-    for i in range(begin_page, end_page+1):  
-        sName = string.zfill(i,5) + '.html'#自动填充成六位的文件名  
-        print '正在下载第' + str(i) + '个网页，并将其存储为' + sName + '......'  
-        f = open(sName,'w+')  
-        m = urllib2.urlopen(url + str(i)).read()  
-        f.write(m)  
-        f.close()  
-   
-   
-#-------- 在这里输入参数 ------------------  
-  
-# 这个是山东大学的百度贴吧中某一个帖子的地址  
-#bdurl = 'http://tieba.baidu.com/p/2296017831?pn='  
-#iPostBegin = 1  
-#iPostEnd = 10  
-  
-bdurl = str(raw_input(u'请输入贴吧的地址，去掉pn=后面的数字：\n'))  
-begin_page = int(raw_input(u'请输入开始的页数：\n'))  
-end_page = int(raw_input(u'请输入终点的页数：\n'))  
-#-------- 在这里输入参数 ------------------  
-   
-  
-#调用  
-baidu_tieba(bdurl,begin_page,end_page) 
+# -*-coding:utf-8 -*-  
+import re
+import urllib
+import urllib2
+
+class ZHIHU():
+    def  __init__(self):
+        self.url = 'https://www.zhihu.com/'
+        self.agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
+        self.headers = {'User_Agent': self.agent}
+    def getPage(self):
+        #print('hehe')
+        try:
+            request = urllib2.Request(self.url, headers =self.headers)
+            response = urllib2.urlopen(request)
+            pageCode = response.read()
+            print(pageCode)
+        except urllib2.URLError,e:
+            if hasattr(e,'reason'):
+                print('连接知乎失败',e.reason)
+                return None
+zhihu = ZHIHU()
+zhihu.getPage()
+        
